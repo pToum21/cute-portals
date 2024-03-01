d806f282ab205386cd72fc9ab137076e448d23ba
toum21 <pey2moo1@gmail.com>
2024-03-01 15:49:21 -0800

    created a textures folder and added skyboxes in it

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,16 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useTexture } from "@react-three/drei";
 
 export const Experience = () => {
+
+  const map = useTexture(
+    'textures/Anime_equirectangular-jpg_desert_sand_dunes_world_1355661936_10321814.jpg'
+  );
   return (
     <>
       <OrbitControls />
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshStandardMaterial map={map} />
       </mesh>
     </>
   );
