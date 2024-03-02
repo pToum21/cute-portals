2c21822c876ca9a9177ea0ce496f45cefe4db9e1
toum21 <pey2moo1@gmail.com>
2024-03-01 16:07:25 -0800

    set the ghosts idle animation

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import { Environment, OrbitControls, useTexture } from "@react-three/drei";
 import * as THREE from "three"
+import { Ghost } from "./Ghost";
 export const Experience = () => {
 
   const map = useTexture(
@@ -10,9 +11,10 @@ export const Experience = () => {
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
       <OrbitControls />
+      <Ghost scale={0.6} />
       <mesh>
         <sphereGeometry args={[5, 64, 64]} />
-        <meshStandardMaterial map={map} side={THREE.BackSide}/>
+        <meshStandardMaterial map={map} side={THREE.BackSide} />
       </mesh>
     </>
   );
