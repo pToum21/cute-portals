159b43017b58eade849ab616356a0e7da782cb89
toum21 <pey2moo1@gmail.com>
2024-03-05 15:47:58 -0800

    added the two other models and coverted them to jsx inside of componets, the frog and yeti

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -14,11 +14,11 @@ export const Experience = () => {
         <Ghost scale={0.6} position-y={-1} />
       </MonsterStage>
       {/* snow penguin */}
-      <MonsterStage texture={"textures/Anime_equirectangular-jpg_snowy_forest_with_big_909903919_10321818.jpg"}>
+      <MonsterStage texture={"textures/Anime_equirectangular-jpg_snowy_forest_with_big_909903919_10321818.jpg"} position-x={-2.5} rotation-y={Math.PI /8}>
         <Ghost scale={0.6} position-y={-1} />
       </MonsterStage>
       {/* water frog */}
-      <MonsterStage texture={"textures/Anime_equirectangular-jpg_water_world_664462261_10321798.jpg"}>
+      <MonsterStage texture={"textures/Anime_equirectangular-jpg_water_world_664462261_10321798.jpg"} position-x={2.5} rotation-y={-Math.PI /8}>
         <Ghost scale={0.6} position-y={-1} />
       </MonsterStage>
     </>
