d45a844bf278fde93d0378715375cef4a4fb3250
toum21 <pey2moo1@gmail.com>
2024-03-05 15:41:32 -0800

    updated the card the model displays in and made it dynamic so it can be a diffrent scene and model

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,21 +1,46 @@
-import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { Environment, MeshPortalMaterial, OrbitControls, RoundedBox, useTexture } from "@react-three/drei";
 import * as THREE from "three"
 import { Ghost } from "./Ghost";
 export const Experience = () => {
 
-  const map = useTexture(
-    'textures/Anime_equirectangular-jpg_desert_sand_dunes_world_1355661936_10321814.jpg'
-  );
+
   return (
     <>
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
       <OrbitControls />
-      <Ghost scale={0.6} />
-      <mesh>
-        <sphereGeometry args={[5, 64, 64]} />
-        <meshStandardMaterial map={map} side={THREE.BackSide} />
-      </mesh>
+      {/* desert ghost */}
+      <MonsterStage texture={"textures/Anime_equirectangular-jpg_desert_sand_dunes_world_1355661936_10321814.jpg"}>
+        <Ghost scale={0.6} position-y={-1} />
+      </MonsterStage>
+      {/* snow penguin */}
+      <MonsterStage texture={"textures/Anime_equirectangular-jpg_snowy_forest_with_big_909903919_10321818.jpg"}>
+        <Ghost scale={0.6} position-y={-1} />
+      </MonsterStage>
+
+      <MonsterStage texture={"textures/Anime_equirectangular-jpg_desert_sand_dunes_world_1355661936_10321814.jpg"}>
+        <Ghost scale={0.6} position-y={-1} />
+      </MonsterStage>
     </>
   );
 };
+
+const MonsterStage = ({ children, texture, ...props }) => {
+  const map = useTexture(texture);
+  return (
+    <group {...props}>
+      <RoundedBox args={[2, 3, 0.1]}>
+        <MeshPortalMaterial side={THREE.DoubleSide}>
+          <ambientLight intensity={1} />
+          <Environment preset="sunset" />
+          {children}
+          <mesh>
+            <sphereGeometry args={[5, 64, 64]} />
+            <meshStandardMaterial map={map} side={THREE.BackSide} />
+          </mesh>
+        </MeshPortalMaterial>
+      </RoundedBox>
+    </group>
+  );
+};
+
